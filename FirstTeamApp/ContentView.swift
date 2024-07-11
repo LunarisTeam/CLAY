d47f9a271bdb257d9fdd3e86e1161098a2cc1e24
Dave-Ed-Cast <148765897+Dave-Ed-Cast@users.noreply.github.com>
2024-07-11 12:41:46 +0200

    New feature:
    - Added the drawing view with the mesh resource
    - Added the pinch components
    - Added the hierarchy navigation
    - Added the immersive space

--- FirstTeamApp/ContentView.swift
+++ FirstTeamApp/ContentView.swift
@@ -10,45 +10,37 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
-
-    @State private var showImmersiveSpace = false
-    @State private var immersiveSpaceIsShown = false
-
+        
     @Environment(\.openImmersiveSpace) var openImmersiveSpace
     @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
+    @Environment(\.dismissWindow) var dismissWindow
 
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
 
-            Text("Hello, world!")
+            Text("Welcome to the drawing app!")
+                .font(.extraLargeTitle)
 
-            Toggle("Show ImmersiveSpace", isOn: $showImmersiveSpace)
-                .font(.title)
-                .frame(width: 360)
-                .padding(24)
-                .glassBackgroundEffect()
-        }
-        .padding()
-        .onChange(of: showImmersiveSpace) { _, newValue in
-            Task {
-                if newValue {
-                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                    case .opened:
-                        immersiveSpaceIsShown = true
-                    case .error, .userCancelled:
-                        fallthrough
-                    @unknown default:
-                        immersiveSpaceIsShown = false
-                        showImmersiveSpace = false
-                    }
-                } else if immersiveSpaceIsShown {
-                    await dismissImmersiveSpace()
-                    immersiveSpaceIsShown = false
+            Button {
+                Task {
+                    
+                    dismissWindow(id: "main")
+                    await openImmersiveSpace(id: "DrawingView")
                 }
+            } label: {
+                Text("Show Drawing View")
+                    .font(.title)
+                    .frame(width: 360)
+                    .padding(24)
+                    .glassBackgroundEffect()
             }
         }
+        
+        .onDisappear {
+            dismissWindow(id: "main")
+        }
+        
+        .padding()
     }
 }
 
--- FirstTeamApp/FirstTeamAppApp.swift
+++ FirstTeamApp/FirstTeamAppApp.swift
@@ -7,15 +7,19 @@
 
 import SwiftUI
 
+let conversionFactorMetersToPixel: CGFloat = 1360
+
 @main
 struct FirstTeamAppApp: App {
     var body: some Scene {
-        WindowGroup {
+        
+        //builds scenes. We need to give the id for the views, because we have to open and dismiss manually
+        WindowGroup(id: "main") {
             ContentView()
         }
-
-        ImmersiveSpace(id: "ImmersiveSpace") {
-            ImmersiveView()
-        }.immersionStyle(selection: .constant(.full), in: .full)
+        //builds immersive spaces, same concept as before
+        ImmersiveSpace(id: "DrawingView") {
+            DrawingView()
+        }
     }
 }
