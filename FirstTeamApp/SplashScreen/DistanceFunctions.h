90bad5d01afa025252e58a356db0abf643fd88e3
Dave-Ed-Cast <148765897+Dave-Ed-Cast@users.noreply.github.com>
2024-07-23 14:29:07 +0200

    Added the files needed

---
+++ FirstTeamApp/SplashScreen/DistanceFunctions.h
@@ -0,0 +1,40 @@
+//
+//  DistanceFunctions.h
+//  FirstTeamApp
+//
+//  Created by Davide Castaldi on 23/07/24.
+//
+
+#pragma once
+
+#include <metal_stdlib>
+ 
+// Returns the distance from a point p to line segment a <-> b.
+// https://iquilezles.org/articles/distfunctions2d/
+inline float distance_to_line_segment(float2 p, float2 a, float2 b)
+{
+    using namespace metal;
+    
+    float2 pa = p - a, ba = b - a;
+    float h = clamp(dot(pa, ba) / dot(ba, ba), 0.0, 1.0);
+    return length(pa - ba * h);
+}
+
+// Returns the distance from a point p to a box with rounded corners.
+// https://iquilezles.org/articles/distfunctions2d/
+//
+// b.x = width
+// b.y = height
+// r.x = roundness top-right
+// r.y = roundness bottom-right
+// r.z = roundness top-left
+// r.w = roundness bottom-left
+inline float signed_distance_to_rounded_box(float2 p, float2 b, float4 r)
+{
+    using namespace metal;
+    
+    r.xy = (p.x > 0.0) ? r.xy : r.zw;
+    r.x  = (p.y > 0.0) ? r.x : r.y;
+    float2 q = abs(p) - b + r.x;
+    return min(max(q.x, q.y), 0.0) + length(max(q, 0.0)) - r.x;
+}
---
+++ FirstTeamApp/SplashScreen/SplashScreenView.swift
@@ -0,0 +1,45 @@
+//
+//  SplashScreenView.swift
+//  FirstTeamApp
+//
+//  Created by Davide Castaldi on 23/07/24.
+//
+
+import RealityKit
+import SwiftUI
+
+struct SplashScreenView: View {
+    private static let startButtonWidth: CGFloat = 150
+    
+    @Environment(\.setMode) var setMode
+    
+    var body: some View {
+        ZStack {
+            SplashScreenBackgroundView()
+            
+            VStack {
+                Spacer(minLength: 100)
+                
+                SplashScreenForegroundView()
+                
+                Spacer(minLength: 50)
+                
+                Button {
+                    Task {
+                        await setMode(.chooseWorkVolume)
+                    }
+                } label: {
+                    Text("Start").frame(minWidth: Self.startButtonWidth)
+                }
+                .glassBackgroundEffect()
+                .controlSize(.extraLarge)
+                .frame(width: Self.startButtonWidth)
+                
+                Spacer(minLength: 100)
+            }
+            .frame(depth: 0, alignment: DepthAlignment.back)
+        }
+        .frame(depth: 100, alignment: DepthAlignment.back)
+    }
+}
+
