d17b8ab66aa55d63b88ed0af1f4fb94b1c28c222
itsalessandroricci <120020257+itsalessandroricci@users.noreply.github.com>
2024-07-23 10:27:16 +0200

    I added the UI

--- FirstTeamApp/UserInterface/DashboardView.swift
+++ FirstTeamApp/UserInterface/DashboardView.swift
@@ -16,11 +16,38 @@ struct DashboardView: View {
     
     var body: some View {
         ZStack {
+            
+            Image("WelcomeImage")
+                .resizable()
+                .frame(width: 950)
+            
+            Image("Clogo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 240)
+                .padding(.trailing, 300)
+                .padding(.bottom, 100)
+                .shadow(radius: 20)
                 
-            // Something goes here (backdrop maybe)
+            
+            Text("LAY")
+                .font(.system(size: 100, weight: .thin, design: .rounded))
+                .fontWeight(.thin)
+                .foregroundColor(Color.black)
+                .kerning(60)
+                .padding(.leading, 270)
+                .padding(.bottom, 100)
+            
+            Text("Spatial Drawing\nfor VisionOs")
+                .font(.system(size: 34, weight: .thin, design: .rounded))
+                .fontWeight(.light)
+                .foregroundColor(Color.black)
+                .multilineTextAlignment(.center)
+                .padding(.top, 150)
+                
             
             VStack {
-                Spacer(minLength: 100)
+                Spacer(minLength: 500)
                 
                 // Something goes here (logo maybe)
                 
@@ -33,15 +60,23 @@ struct DashboardView: View {
                     Task {
                         await openImmersiveSpace(id: "DrawingView")
                     }
+                    
                 } label: {
                     Text("Start").frame(minWidth: 150)
                 }
+                
+                .shadow( radius: 20)
+                .opacity(1)
                 .glassBackgroundEffect()
                 .controlSize(.extraLarge)
                 
                 Spacer(minLength: 100)
             }
+            
         }
     }
 }
 
+#Preview {
+    DashboardView()
+}
